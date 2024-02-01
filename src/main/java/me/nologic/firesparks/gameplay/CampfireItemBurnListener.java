b0277360017542476b9ce2487ba441dd9fe9c2db
Rin Isayo <156855210+rindefault@users.noreply.github.com>
2024-02-01 23:48:49 +0300

    Ehhh....

--- src/main/java/me/nologic/firesparks/gameplay/CampfireItemBurnListener.java
+++ src/main/java/me/nologic/firesparks/gameplay/CampfireItemBurnListener.java
@@ -1,384 +1,151 @@
 package me.nologic.firesparks.gameplay;
 
-import dev.geco.gsit.api.GSitAPI;
 import me.nologic.firesparks.Firesparks;
+import me.nologic.firesparks.gameplay.events.ItemBurnEvent;
+import me.nologic.firesparks.utilities.Colors;
 import me.nologic.firesparks.utilities.Configuration;
-import net.kyori.adventure.bossbar.BossBar;
+import me.nologic.firesparks.utilities.items.BurnableItem;
+import me.nologic.firesparks.utilities.items.BurnableItemList;
 import net.kyori.adventure.text.Component;
 import org.bukkit.*;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.type.Campfire;
-import org.bukkit.block.data.type.Furnace;
-import org.bukkit.entity.Entity;
+import org.bukkit.block.Campfire;
+import org.bukkit.entity.AreaEffectCloud;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemDisplay;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Map;
+import javax.annotation.Nullable;
+import java.util.List;
 
-public class ComfortDataHandler extends Thread implements Listener {
+public class CampfireItemBurnListener implements Listener {
 
-    private final Firesparks plugin;
-    private final Map<Player, ComfortData> comfortDataMap;
-    private boolean cancelled;
-    private final int comfortGrowthAmount;
-    
-    @Override
-    public void run() {
-        while (!this.cancelled) {
-            this.tick();
-            try { Thread.sleep(2000L); }
-            catch (Exception ignored) {}
-        }
-    }
-    
-    private void tick() {
-        this.comfortDataMap.values().forEach(data -> {
-
-            data.update();
-
-            if (data.isDelightfulness()) {
-
-                Player player = data.getPlayer();
-
-                Bukkit.getServer().getScheduler().runTask(this.plugin, () -> {
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 50, 0, true, true, true));
-                    player.setSaturation(player.getSaturation() - 1.0f);
-
-                    if (player.getSaturation() == 0.0f) {
-                        player.setFoodLevel(player.getFoodLevel() - 1);
-                    }
-
-                    if (!data.isMusicPlaying()) {
-                        player.playSound(player.getLocation(), Sound.MUSIC_DISC_CAT, 1.0f, 1.0f);
-                        data.setMusicPlaying(true);
-                    }
-
-                });
-            }
-        });
-    }
-    
-    @EventHandler
-    private void whenPlayerJoin(final PlayerJoinEvent event) {
-        final Component name = Component.text(Configuration.getComfortBarName());
-        final BossBar personalPlayerBar = BossBar.bossBar(name, 0.0f, BossBar.Color.RED, BossBar.Overlay.NOTCHED_20);
-        final ComfortData data = new ComfortData(event.getPlayer(), personalPlayerBar);
-        this.comfortDataMap.put(event.getPlayer(), data);
-    }
-    
-    @EventHandler
-    private void whenPlayerQuit(final PlayerQuitEvent event) {
-        this.comfortDataMap.remove(event.getPlayer());
-    }
-    
-    @EventHandler
-    private void whenPlayerTakesDamage(final EntityDamageEvent event) {
-        final Entity entity = event.getEntity();
-        if (entity instanceof Player player) {
-            final ComfortData data = this.comfortDataMap.get(player);
-            if (data != null) {
-                data.setLevel((int)(data.getLevel() - event.getFinalDamage() * 50.0));
-                data.getBar().progress((float)(data.getLevel() / 1000));
-            }
-        }
-    }
+    private final BurnableItemList burnableItemList = Configuration.getItemList();
 
     @EventHandler
-    private void whenPlayerRespawns(final PlayerRespawnEvent event) {
+    public void onCampfireInteract(final PlayerInteractEvent event) {
 
-        if(!Configuration.getColdness()) return;
+        if (event.getPlayer().getGameMode().equals(GameMode.SPECTATOR)) return;
+        if (event.getHand() == null) return;
+        if (!event.getHand().equals(EquipmentSlot.HAND)) return;
+        if (!event.getAction().isRightClick()) return;
+        if (event.getClickedBlock() == null) return;
+        if (!event.getClickedBlock().getType().equals(Material.CAMPFIRE)) return;
 
-        final Player player = event.getPlayer();
-        if(player.getFreezeTicks() > 0) {
-            new BukkitRunnable() {
+        Player player = event.getPlayer();
+        ItemStack itemInHand = player.getInventory().getItemInMainHand();
+        Campfire campfire = (Campfire) event.getClickedBlock().getState();
 
-                @Override
-                public void run() {
-                    player.setFreezeTicks(0);
+        for (BurnableItem burnableItem : burnableItemList.getItems()) {
+
+            Material itemMaterial = burnableItem.getMaterial();
+
+            if (itemInHand.getType().equals(itemMaterial)) {
+
+                event.setCancelled(true);
+
+                if (
+                    getCampfireItem(event.getClickedBlock().getLocation()) != null ||
+                    isCampfireHasFood(campfire)
+                ) {
+                    player.sendMessage(Configuration.getMessage("campfire-no-capacity"));
+                    return;
                 }
 
-            }.runTask(plugin);
+                if (!player.getGameMode().equals(GameMode.CREATIVE)) {
+                    itemInHand.setAmount(itemInHand.getAmount() - 1);
+                }
+
+                burn(burnableItem, player, event.getClickedBlock().getLocation());
+
+            }
         }
     }
-    
-    public ComfortDataHandler(final Firesparks plugin, final Map<Player, ComfortData> comfortDataMap) {
-        this.plugin = plugin;
-        this.comfortDataMap = comfortDataMap;
-        this.comfortGrowthAmount = Configuration.getComfortGrowthAmount();
-    }
-    
-    public void setCancelled(final boolean cancelled) {
-        this.cancelled = cancelled;
-    }
-    
-    public final class ComfortData {
+    @EventHandler
+    public void onCampfireBreak(BlockBreakEvent event) {
 
-        private Coldness coldness = Coldness.NORMAL;
-        private Boolean coldnessTask = false;
+        Location location = event.getBlock().getLocation();
 
-        private final Player player;
-        private int level;
-        private Level state;
-        private boolean musicPlaying;
-        private boolean nearFire;
-        private boolean hasArmor;
-        private boolean sitting;
-        private final BossBar bar;
-        
-        public void update() {
-
-            final boolean wellFed = this.player.getFoodLevel() > 17;
-            this.nearFire = this.isPlayerNearWarmBlock();
-            this.sitting = GSitAPI.isSitting(this.player);
-            this.hasArmor = this.isPlayerHasArmor();
-
-            if (this.nearFire) {
-                this.coldness = Coldness.WARM;
-                if (this.sitting && wellFed) {
-                    this.increment();
-                }
-            }
-            else {
-                if (Configuration.getColdness()) {
-                    if (this.isPlayerInColdBiome()) {
-                        decrementColdness();
-                    }
-                    else {
-                        resetColdness();
-                    }
-                }
-                this.decrement();
-            }
-        }
-        
-        private void restate() {
-            if (this.level < 100) {
-                this.state = Level.LOW;
-            }
-            else if (this.level < 600) {
-                this.state = Level.NORMAL;
-                this.musicPlaying = false;
-            }
-            else if (this.level < 800) {
-                if (this.state == Level.COZY) {
-                    this.player.sendMessage(Configuration.getMessage("delightfulness"));
-                }
-                this.state = Level.NICE;
-            }
-            else {
-                this.state = Level.COZY;
-            }
-        }
-        
-        public boolean isDelightfulness() {
-            return this.state == Level.COZY && this.nearFire && this.sitting;
-        }
-        
-        public void setLevel(int value) {
-            if (value < 0) {
-                value = 0;
-            }
-            else if (value > 1000) {
-                value = 1000;
-            }
-            this.level = value;
-        }
-        
-        public void showBossBar(final float level) {
-
-            switch (this.state) {
-                case LOW -> this.bar.color(BossBar.Color.RED);
-                case NORMAL -> this.bar.color(BossBar.Color.YELLOW);
-                case NICE -> this.bar.color(BossBar.Color.BLUE);
-                case COZY -> this.bar.color(BossBar.Color.GREEN);
-            }
-
-            this.bar.progress(level / 1000.0f);
-            this.player.showBossBar(this.bar);
-            Bukkit.getServer().getScheduler().runTaskLater(ComfortDataHandler.this.plugin, () -> {
-                if (level >= this.level) {
-                    this.player.hideBossBar(this.bar);
-                }
-            }, 100L);
-        }
-        
-        private void increment() {
-            if (this.level != 1000) {
-                if (this.level + comfortGrowthAmount > 1000) {
-                    this.level = 1000;
-                }
-                else {
-                    this.level += comfortGrowthAmount;
-                }
-                this.restate();
-                this.showBossBar((float)this.level);
-            }
-        }
-        
-        private void decrement() {
-            if (this.level != 0) {
-                if (this.level - 2 < 0) {
-                    this.level = 0;
-                }
-                else {
-                    this.level -= 2;
-                }
-                this.restate();
-            }
+        if (getCampfireItem(location) != null && event.getBlock().getType().equals(Material.CAMPFIRE)) {
+            getCampfireItem(location).remove();
         }
 
-        private void resetColdness() {
-
-            if (this.coldness == Coldness.COLD) {
-                this.coldness = Coldness.NORMAL;
-            }
-        }
-        private void decrementColdness() {
-
-            if (
-                player.getGameMode().equals(GameMode.SPECTATOR) ||
-                player.getGameMode().equals(GameMode.CREATIVE)
-            ) return;
-
-            if (this.hasArmor) return;
-
-            if (this.coldness == Coldness.WARM) {
-                this.coldness = Coldness.NORMAL;
-            }
-            else if (this.coldness == Coldness.NORMAL) {
-
-                if (player.getFreezeTicks() > 0) return;
-                if(this.coldnessTask) return;
-
-                new BukkitRunnable() {
-                    int freezeTime = 0;
-
-                    @Override
-                    public void run() {
-                        coldnessTask = true;
-
-                        if (
-                            coldness == Coldness.COLD ||
-
-                            !isPlayerInColdBiome() ||
-                            isPlayerHasArmor() ||
-                            isPlayerNearWarmBlock()
-                        ) {
-                            this.cancel();
-                            coldnessTask = false;
-                        }
-
-                        if (
-                            player.getGameMode().equals(GameMode.SPECTATOR) ||
-                            player.getGameMode().equals(GameMode.CREATIVE)
-                        ) {
-                            this.cancel();
-                            resetColdness();
-                            coldnessTask = false;
-                        }
-
-                        if (freezeTime < 120) {
-                            freezeTime++;
-
-                            player.setFreezeTicks(freezeTime);
-                        }
-                        else {
-                            this.cancel();
-                            coldnessTask = false;
-                            coldness = Coldness.COLD;
-
-                            player.setFreezeTicks(200);
-
-                            player.sendMessage(Configuration.getMessage("getting-cold"));
-                            player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_HURT_FREEZE, 1f, 0f);
-                        }
-                    }
-
-                }.runTaskTimer(plugin, 0L, 1L);
-            }
-            else if (this.coldness == Coldness.COLD) {
-                player.setFreezeTicks(200);
-            }
-
-        }
-        
-        private boolean isPlayerNearWarmBlock() {
-            final Material[] targets = { Material.CAMPFIRE, Material.SOUL_CAMPFIRE, Material.FURNACE, Material.BLAST_FURNACE, Material.SMOKER };
-            final Location base = this.player.getLocation();
-            for (int y = -2; y < 2; ++y) {
-                final Location loc = new Location(base.getWorld(), base.getX(), base.getY() + y, base.getZ());
-                for (int distance = 1; distance < 5; ++distance) {
-                    for (final BlockFace face : BlockFace.values()) {
-                        final Block block = loc.getBlock().getRelative(face, distance);
-                        for (final Material target : targets) {
-                            if (block.getType() == target) {
-                                return switch (target) {
-                                    case CAMPFIRE, SOUL_CAMPFIRE -> ((Campfire) block.getBlockData()).isLit();
-                                    case FURNACE, BLAST_FURNACE, SMOKER -> ((Furnace) block.getBlockData()).isLit();
-                                    default -> false;
-                                };
-                            }
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-        private boolean isPlayerHasArmor() {
-            return this.player.getInventory().getBoots() != null &&
-                   this.player.getInventory().getLeggings() != null &&
-                   this.player.getInventory().getChestplate() != null &&
-                   this.player.getInventory().getHelmet() != null;
-        }
-        private boolean isPlayerInColdBiome() {
-            return this.player.getWorld().getBlockAt(this.player.getLocation()).getTemperature() <= 0.05;
-        }
-        
-        public ComfortData(final Player player, final BossBar bar) {
-            this.player = player;
-            this.bar = bar;
-        }
-        
-        public Player getPlayer() {
-            return this.player;
-        }
-        
-        public int getLevel() {
-            return this.level;
-        }
-
-        public boolean isMusicPlaying() {
-            return this.musicPlaying;
-        }
-        
-        public void setMusicPlaying(final boolean musicPlaying) {
-            this.musicPlaying = musicPlaying;
-        }
-        
-        public BossBar getBar() {
-            return this.bar;
-        }
-        
-        public enum Level {
-            LOW, 
-            NORMAL, 
-            NICE, 
-            COZY
-        }
-        public enum Coldness {
-            COLD,
-            NORMAL,
-            WARM
-        }
     }
 
+    private void burn(final BurnableItem burnableItem, final Player player, final Location location) {
+
+        ItemDisplay itemDisplay = (ItemDisplay) location.getWorld().spawnEntity(location.clone().add(0.1, 0.5, 0.4), EntityType.ITEM_DISPLAY);
+        itemDisplay.setItemStack(new ItemStack(burnableItem.getMaterial(), 1));
+        itemDisplay.setRotation(45f, 90f);
+        itemDisplay.customName(Component.text("CampfireItem"));
+
+        location.getWorld().playSound(location, Sound.BLOCK_FIRE_EXTINGUISH, 1f, 1f);
+
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+
+                if (getCampfireItem(location) != null) {
+
+                    int[] colors = Colors.fromString(burnableItem.getPotionColor());
+
+                    location.getWorld().playSound(location, Sound.ENTITY_SHULKER_BULLET_HURT, 1f, 1f);
+                    location.getWorld().playSound(location, Sound.ENTITY_SHULKER_SHOOT, 1f, 1f);
+                    itemDisplay.remove();
+
+                    AreaEffectCloud areaEffectCloud = (AreaEffectCloud) location.getWorld().spawnEntity(location.toCenterLocation(), EntityType.AREA_EFFECT_CLOUD);
+
+                    PotionEffect potionEffect = new PotionEffect(burnableItem.getPotionEffect(), 20*burnableItem.getDuration(), 0);
+                    Particle.DustOptions dustOptions = new Particle.DustOptions(Color.fromRGB(colors[0], colors[1], colors[2]), 1);
+
+                    areaEffectCloud.setParticle(Particle.REDSTONE, dustOptions);
+                    areaEffectCloud.setRadius(5f);
+                    areaEffectCloud.setRadiusPerTick(-0.005f);
+                    areaEffectCloud.addCustomEffect(potionEffect, false);
+
+                    for (Object player : location.getNearbyPlayers(10).toArray()) {
+                        ((Player) player).addPotionEffect(PotionEffectType.CONFUSION.createEffect(20 * 25, 0));
+                        ((Player) player).sendMessage(Configuration.getMessage("item-burn"));
+                    }
+
+                    ItemBurnEvent itemBurnEvent = new ItemBurnEvent(burnableItem, player);
+                    Bukkit.getPluginManager().callEvent(itemBurnEvent);
+                }
+
+                this.cancel();
+            }
+        }.runTaskLater(Firesparks.getPlugin(), 20L * burnableItem.getTimeToBurn());
+    }
+
+    @Nullable
+    private ItemDisplay getCampfireItem(@NotNull final Location location) {
+
+        List<ItemDisplay> itemDisplayList = location
+                .getNearbyEntitiesByType(ItemDisplay.class, 1)
+                .stream()
+                .filter(itemDisplay -> itemDisplay.name().equals(Component.text("CampfireItem")))
+                .toList();
+
+        return itemDisplayList.isEmpty() ? null : itemDisplayList.get(0);
+    }
+    private boolean isCampfireHasFood(@NotNull final Campfire campfire) {
+        for (int i = 0; i < campfire.getSize(); i++) {
+
+            if (campfire.getItem(i) != null) {
+                return true;
+            }
+
+        }
+        return false;
+    }
 }
